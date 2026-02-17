05a591308957fabd2da69aa6ea3e74d273459d10
userName <userEmail>
2026-02-17 19:17:18 +0800

    update

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import paramiko
 # 实例化SSHClient
 client = paramiko.SSHClient()
 
+
 # 自动添加策略，保存服务器的主机名和密钥信息，如果不添加，那么不再本地know_hosts文件中记录的主机将无法连接
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
  
